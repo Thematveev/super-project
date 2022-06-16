8b945ed85ef6b33dc35108a95bbe3063fc5ab661
Thematveev <33023031+Thematveev@users.noreply.github.com>
2022-06-16 19:03:00 +0300

    loops

---
+++ script.js
@@ -0,0 +1,22 @@
+for (let i = 0; i < 10; i++) {
+    console.log("Hello world", i);
+
+    if (i == 5) {
+        continue;
+    }
+
+    console.log("End");
+}
+
+// let number = 0;
+
+// while (number == 0) {
+//     number = prompt("Number:");
+// }
+
+// let number = 123;
+
+// do {
+//     console.log("Hello");
+//     number++;
+// } while (number < 10);
