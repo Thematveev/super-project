725c7e6040ee9accb7fe5719fb52dd3c78192bfa
Thematveev <33023031+Thematveev@users.noreply.github.com>
2022-06-16 18:46:17 +0300

    if statement

---
+++ script.js
@@ -0,0 +1,15 @@
+let weather = 100;
+
+if (weather == 1) {
+    console.log("Sun");
+} else if (weather == 2) {
+    console.log("Rain");
+} else if (weather == 3) {
+    console.log("Snow");
+} else {
+    console.log("Error in weather!");
+}
+
+let result = 2 === "2";
+
+console.log(result);
